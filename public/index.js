61412507e76dbc4244eb29a15c0ec0c5204a067e
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-04-02 14:32:05 +0200

    add index

---
+++ public/index.js
@@ -0,0 +1,9 @@
+let cheminUrl = window.location.pathname;
+if (cheminUrl.includes("/homepage/homepage.html")){
+    import("./../templates/homepage/homepage.js");
+} else if (cheminUrl.includes("gallery/gallery.html")){
+    import("./../templates/gallery/gallery.js");
+} else if (cheminUrl.includes("games/games.html")){
+    import("./../templates/games/games.js");
+}
+ import("./../layouts/navbar.js");
