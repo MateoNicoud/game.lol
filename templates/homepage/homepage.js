a0f14ef59804990cf4006bc59d9cc13d5d6818d3
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-04-02 15:52:01 +0200

    created homepage

--- templates/homepage/homepage.js
+++ templates/homepage/homepage.js
@@ -1,1 +1,5 @@
-console.log(window.location.pathname)
+fetch("https://tyradex.tech/api/v1/pokemon")
+    .then(response => response.json()) // transforme le résultat de l'API en JSON
+    .then(response2 => {
+
+    })
