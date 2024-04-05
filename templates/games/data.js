f443250836a876f4cf49401ccd5fab022a0bcbbe
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-04-05 15:01:49 +0200

    #6 added games page html/css

---
+++ templates/games/data.js
@@ -0,0 +1,9 @@
+let jsonDatas=[
+  {
+    "name" :"Rock, Paper, Scissors !!!",
+    "type" : "action",
+    "description" : "Play rock, parper, scissors versus computer",
+    "urlImage" : "./rockParperScissors/rock-paper-scissors.png",
+    "urlGame":"./rockParperScissors/rockParperScissors.js"
+  }
+]
