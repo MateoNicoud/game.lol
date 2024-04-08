60de338ce4e93ee7b54fb173c973e06e8de045c0
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-04-08 14:29:57 +0200

    #37 fixed forms

--- templates/homepage/homepage.js
+++ templates/homepage/homepage.js
@@ -1,48 +1,35 @@
 let feed = document.getElementById("feed");
 let form = document.getElementById("form");
+let addedPokemon =document.getElementById("addedPokemon")
 fetch("https://tyradex.tech/api/v1/pokemon")
     .then(response => response.json()) // transforme le résultat de l'API en JSON
     .then(response2 => {
         console.log(response2);
-        let inputName = document.createElement("input")
-        let inputSprite = document.createElement("input")
-        let submit = document.createElement("button")
-        submit.textContent = "Submit"
-        inputName.setAttribute("type", "text")
-        inputName.setAttribute("placeholder", "give a pokemon name")
-        inputSprite.setAttribute("type", "url")
-        inputSprite.setAttribute("placeholder", "give a url of sprite")
-        form.appendChild(inputName);
-        form.appendChild(inputSprite);
-        form.appendChild(submit);
-        submit.onclick = function () {
-            console.log(inputName.value && inputSprite.value);
-            if (inputName.value !== "" && inputSprite.value !== "") {
-                let pokemon = document.createElement("div");
-                form.appendChild(pokemon);
+        let inputName = document.getElementById("inputName")
+        let inputSprite = document.getElementById("inputSprite")
+        form.onsubmit = function () {
+            let pokemon = document.createElement("div");
+            addedPokemon.appendChild(pokemon);
 
-                let destroyButton =document.createElement("button");
-                destroyButton.textContent="x"
-                pokemon.appendChild(destroyButton)
+            let destroyButton = document.createElement("button");
+            destroyButton.textContent = "x"
+            pokemon.appendChild(destroyButton)
 
-                destroyButton.onclick = function (){
-                    form.removeChild(destroyButton.parentNode)
-                }
-
-                let showName = document.createElement("h3");
-                let name = inputName.value;
-                showName.textContent = name;
-                pokemon.setAttribute('name', name);
-                pokemon.appendChild(showName);
-                let showSprite = document.createElement("img");
-                let sprite = inputSprite.value;
-                showSprite.setAttribute('src', sprite);
-                pokemon.appendChild(showSprite);
-            } else {
-                let error = document.createElement("span")
-                error.textContent = "fill all input";
-                form.appendChild(error)
+            destroyButton.onclick = function () {
+                addedPokemon.removeChild(destroyButton.parentNode)
             }
+
+            let showName = document.createElement("h3");
+            let name = inputName.value;
+            showName.textContent = name;
+            pokemon.setAttribute('name', name);
+            pokemon.appendChild(showName);
+            let showSprite = document.createElement("img");
+            let sprite = inputSprite.value;
+            showSprite.setAttribute('src', sprite);
+            pokemon.appendChild(showSprite);
+            return false;
+
         }
         for (let i = 0; i < 3; i++) {
             let randomPokemon = Math.floor(Math.random() * response2.length)
